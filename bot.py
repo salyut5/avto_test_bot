1876cf129d245bae73466743a66b170c775ef070
salyut5 <sadullasaidov70@gmail.com>
2026-04-09 23:01:41 +0500

    mavxular bo'lmaganda bot uxlab qoldi

--- bot.py
+++ bot.py
@@ -1297,6 +1297,7 @@ async def finish_mixed_test(message, state: FSMContext):
         reply_markup=kb
     )
 
+# ================= ASOSIY MENYUGA QAYTISH =================
 @dp.message_handler(lambda m: m.text == "🏠 Asosiy menyuga qaytish", state="*")
 async def back_to_main(message: types.Message, state: FSMContext):
     try:
@@ -1306,7 +1307,6 @@ async def back_to_main(message: types.Message, state: FSMContext):
 
     await message.answer("🏠 Asosiy menyu", reply_markup=main_menu())
 
-# ================= ASOSIY MENYUGA QAYTISH =================
 
 # ================= RUN BERVOMIZ SHOTTAN =================
 
